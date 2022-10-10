be71f8f92f59829702b1769e850a3f77b5e9e3bf
Toru3 <ryuutet@gmail.com>
2022-10-10 15:54:13 +0900

    uncomment file gen

--- make_test_data.sh
+++ make_test_data.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -eu
-#mkdir -p test_data/
-#cd test_data/
-#seq 1000000 | xargs -P"$(nproc)" -I{} sh -c 'echo {}.txt; head -c 512 /dev/urandom 2>/dev/null | base64 >{}.txt'
-#cd ..
-#tar cvf test_data.tar test_data/
-#for c in gzip bzip2 xz zst; do
-#    "$c" -k test_data.tar &
-#done
+mkdir -p test_data/
+cd test_data/
+seq 1000000 | xargs -P"$(nproc)" -I{} sh -c 'echo {}.txt; head -c 512 /dev/urandom 2>/dev/null | base64 >{}.txt'
+cd ..
+tar cvf test_data.tar test_data/
+for c in gzip bzip2 xz zst; do
+    "$c" -k test_data.tar &
+done
 tar zcf test_data2.tar.gz -H oldgnu test_data/ &
 tar zcf test_data3.tar.gz -H pax    test_data/ &
 tar zcf test_data4.tar.gz -H ustar  test_data/ &
